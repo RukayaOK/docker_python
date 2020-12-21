e48a521061bfc87778d03ac557b3c9b6a2b4e759
RukayaOK <rukayaogazikhan@yahoo.co.uk>
2020-12-21 10:57:46 +0000

    initial commit

---
+++ v1/__init__.py
@@ -0,0 +1,13 @@
+from flask import Flask, render_template
+import os
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('home.html')
+
+@app.route('/environment')
+def environment():
+    environment = os.environ['ENV_VAR']
+    return render_template('environment.html', environment = environment)
